91cf7f28c9cca0ea68e401fc0517b8a569b110d9
tc <307301662@qq.com>
2018-09-06 09:36:29 +0800

    modify gateway source

--- Finchley/project1/gateway/src/test/java/com/zl/gateway/GatewayApplicationTests.java
+++ Finchley/project1/gateway/src/test/java/com/zl/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zl;
+package com.zl.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZlApplicationTests {
+public class GatewayApplicationTests {
 
     @Test
     public void contextLoads() {
